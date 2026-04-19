e6e20c025f9f2d18e2bc95b65739933f671cc8ec
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-19 20:15:33 +0800

    fs/proc: Hide lineage paths and fake zygote cache flags
    * Dont allow paths with lineage to be listed
    * Fake all lineage
    * Fake all lineage symlinks
    * Fake jit-zygote-cache flags
    backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -355,43 +355,80 @@ static void show_vma_header_prefix(struct seq_file *m,
 	seq_putc(m, ' ');
 }
 
+static void show_vma_header_prefix_fake(struct seq_file *m,
+				   unsigned long start, unsigned long end,
+				   vm_flags_t flags, unsigned long long pgoff,
+				   dev_t dev, unsigned long ino)
+{
+	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
+	seq_printf(m, "%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu ",
+		   start,
+		   end,
+		   flags & VM_READ ? 'r' : '-',
+		   flags & VM_WRITE ? 'w' : '-',
+		   flags & VM_EXEC ? '-' : '-',
+		   flags & VM_MAYSHARE ? 's' : 'p',
+		   pgoff,
+		   MAJOR(dev), MINOR(dev), ino);
+}
+
 static void
 show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 {
-	struct mm_struct *mm = vma->vm_mm;
-	struct file *file = vma->vm_file;
-	vm_flags_t flags = vma->vm_flags;
-	unsigned long ino = 0;
-	unsigned long long pgoff = 0;
-	unsigned long start, end;
-	dev_t dev = 0;
-	const char *name = NULL;
+        struct mm_struct *mm = vma->vm_mm;
+        struct file *file = vma->vm_file;
+        vm_flags_t flags = vma->vm_flags;
+        unsigned long ino = 0;
+        unsigned long long pgoff = 0;
+        unsigned long start, end;
+        dev_t dev = 0;
+        const char *name = NULL;
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	char *spoofed_redirected_name = NULL;
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIREC
+        char *spoofed_redirected_name = NULL;
+#endif
 
-	if (file) {
-		struct inode *inode = file_inode(vma->vm_file);
+        if (file) {
+                struct inode *inode = file_inode(vma->vm_file);
+                struct dentry *dentry = file->f_path.dentry;
+
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-		if (SUSFS_IS_INODE_OPEN_REDIRECT(inode)) {
-			if (!susfs_open_redirect_spoof_show_map_vma(inode, &ino, &dev, spoofed_redirected_name)) {
-				pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-				goto orig_flow;
-			}
-		}
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
+                if (SUSFS_IS_INODE_OPEN_REDIRECT(inode)) {
+                        if (!susfs_open_redirect_spoof_show_map_vma(inode, &ino, &dev, &spoofed_redirected_name)) {
+                                pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+                                goto orig_flow;
+                        }
+                }
+#endif
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
-		if (SUSFS_IS_INODE_SUS_MAP(inode))
-			return;
+                if (SUSFS_IS_INODE_SUS_MAP(inode))
+                        return;
 #endif
-		dev = inode->i_sb->s_dev;
-		ino = inode->i_ino;
-		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+                dev = inode->i_sb->s_dev;
+                ino = inode->i_ino;
+                pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-		susfs_show_map_vma_spoofer(inode, &dev, &ino);
+                susfs_show_map_vma_spoofer(inode, &dev, &ino);
 #endif
-	}
+                if (dentry) {
+                        const char *path = (const char *)dentry->d_name.name;
+                        if (strstr(path, "lineage")) {
+                                start = vma->vm_start;
+                                end = vma->vm_end;
+                                show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
+                                name = "/system/framework/framework-res.apk";
+                                goto done;
+                        }
+
+                        if (strstr(path, "jit-zygote-cache")) {
+                                start = vma->vm_start;
+                                end = vma->vm_end;
+                                show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
+                                goto bypass;
+                        }
+                }
+        }
+
 
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 orig_flow:
@@ -401,6 +438,7 @@ orig_flow:
 	end = vma->vm_end;
 	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
 
+bypass:
 	/*
 	 * Print the dentry name for named mappings, and a
 	 * special [heap] marker for the heap:
